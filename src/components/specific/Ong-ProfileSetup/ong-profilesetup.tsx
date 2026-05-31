c6889d17c3aa05f281c0cd80e7a53867a24113ab
PauloRC0 <prcs3@discente.ifpe.edu.br>
2026-05-31 15:43:25 -0300

    feat : updating deploy

--- src/components/specific/Ong-ProfileSetup/ong-profilesetup.tsx
+++ src/components/specific/Ong-ProfileSetup/ong-profilesetup.tsx
@@ -383,6 +383,7 @@ export default function OngSetupProfile() {
               />
             </FormSection>
           </div>
+          
 
           <FormSection title="Canais de Contato *" italicTitle>
             <div className="space-y-3 sm:space-y-4">
